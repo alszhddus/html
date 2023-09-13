35d467c4045436ab5470e9a8fa5472376629176d
rami <alszhddus@nate.com>
2023-09-13 17:58:38 +0900

    first commit

--- composecoffee/js/composecoffee(menupage).js
+++ composecoffee/js/composecoffee(menupage).js
@@ -102,9 +102,11 @@ $(function () {
     .hover(
       function () {
         $(".sub_lnb > ul > li ul:first").css("visibility", "visible");
+        $(".content").css("z-index", "-1");
       },
       function () {
         $(".sub_lnb > ul > li ul:first").css("visibility", "hidden");
+        $(".content").css("z-index", "1");
       }
     );
   $(".brand li")
@@ -112,128 +114,56 @@ $(function () {
     .hover(
       function () {
         $(".sub_lnb > ul > li ul").eq(1).css("visibility", "visible");
+        $(".content").css("z-index", "-1");
       },
       function () {
         $(".sub_lnb > ul > li ul").eq(1).css("visibility", "hidden");
+        $(".content").css("z-index", "1");
       }
     );
   $(".sub_lnb > ul > li ul").hover(
     function () {
       $(this).eq(0).css("visibility", "visible");
+      $(".content").css("z-index", "-1");
     },
     function () {
       $(this).eq(0).css("visibility", "hidden");
+      $(".content").css("z-index", "1");
     }
   );
   $(".sub_lnb > ul > li ul").hover(
     function () {
       $(this).eq(1).css("visibility", "visible");
+      $(".content").css("z-index", "-1");
     },
     function () {
       $(this).eq(1).css("visibility", "hidden");
+      $(".content").css("z-index", "1");
     }
   );
   // sub_lnb hover event end
 
-  // title scroll event start
-  $(".title h1 p").css("top", "-117px");
-  $(window).on("mousewheel", function () {
-    console.log(event.wheelDelta);
-    if (event.wheelDelta < 0) {
-      $(".title h1 p").eq(0).css("top", "0px");
-      $(".title h1 p").eq(1).css("top", "0px");
-      $(".title h1 p").eq(2).css("top", "0px");
-    }
-    $(".title .bar").delay(500).animate({ width: "500px" }, 1000, "swing");
-    $(".title .circle").addClass("on");
-  });
-  // title scroll event end
+  // menu click event start
 
-  //con_desc blur scroll event start
-  $(window).scroll(function (e) {
-    let ht = $(window).scrollTop();
-    console.log("스크롤탑 : " + ht);
-    if (ht >= 5200) {
-      $(".con_desc img").addClass("on");
-    }
+  $(".menu ul li").click(function () {
+    let i = $(this).index();
+    console.log(i);
+    $(".menu ul li a").css({ background: "#fff", color: "#2c3e50" });
+    $(".menu ul li")
+      .eq(i)
+      .find("a")
+      .css({ background: "#2c3e50", color: "#fff" });
   });
-  //con_desc blur scroll event end
+  // menu click event end
 
-  // particle bg start
-  particlesJS("particles-js", {
-    particles: {
-      number: { value: 20, density: { enable: true, value_area: 800 } },
-      color: { value: "#a2a2a2" },
-      shape: {
-        type: "circle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 },
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#a2a2a2",
-        opacity: 0.4,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 2,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: { enable: false, rotateX: 600, rotateY: 1200 },
-      },
+  // product_desc hover event start
+  $(".product ul li").hover(
+    function () {
+      $(this).find(".prd_desc").css("opacity", "1");
     },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: false, mode: "repulse" },
-        onclick: { enable: false, mode: "push" },
-        resize: true,
-      },
-      modes: {
-        grab: { distance: 400, line_linked: { opacity: 1 } },
-        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-        repulse: { distance: 200, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 },
-      },
-    },
-    retina_detect: true,
-  });
-  var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
-  update = function () {
-    stats.begin();
-    stats.end();
-    if (
-      window.pJSDom[0].pJS.particles &&
-      window.pJSDom[0].pJS.particles.array
-    ) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    function () {
+      $(".product ul li .prd_desc").css("opacity", "0");
     }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
-  // particle bg end
+  );
+  // product_desc hover event end
 });
