0d797e10998371919174455af38e57804a166ab3
rami <alszhddus@nate.com>
2023-09-14 16:56:44 +0900

    first commit

--- composecoffee/js/composecoffee(mainpage).js
+++ composecoffee/js/composecoffee(mainpage).js
@@ -160,4 +160,34 @@ $(function () {
     },
   });
   // rollingbanner swiper end
+
+  // storybg scroll event start
+  /* $(".story").css("background-position", "0 0");
+  //Everytime we scroll, it will fire the function
+  $(window).on("scroll mousewheel", function (e) {
+    var scrolled = $(this).scrollTop();
+    console.log(event.wheelDelta);
+    console.log(scrolled);
+    let i = 0;
+    if (event.wheelDelta < 0 && scrolled >= 1490) {
+      $(".story").css("background-position", "0" + -(scrolled * 0.15) + "px");
+    } else if (event.wheelDelta > 0 && scrolled <= 3111) {
+      $(".story").css("background-position", "0", +(scrolled * 0.15) + "px");
+    } */
+  function simpleParallax() {
+    //스크롤된 거리
+    var scrolled = $(window).scrollTop() + 1;
+    console.log(scrolled);
+
+    //백그라운드 스크롤 속도 변경 시 0.3 을 수정
+    $(".story ,.notice").css(
+      "background-position",
+      "0" + -(scrolled * 0.08) + "px"
+    );
+  }
+  //Everytime we scroll, it will fire the function
+  $(window).scroll(function (e) {
+    simpleParallax();
+  });
+  // storybg scroll event end
 });
